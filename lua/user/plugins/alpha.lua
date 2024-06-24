c694ff75270e3bb359db7d77eefb8c9f23ea669a
Franco74162 <>
2024-06-24 17:45:43 -0600

    better alpha.lua

--- lua/user/plugins/alpha.lua
+++ lua/user/plugins/alpha.lua
@@ -14,7 +14,12 @@ return {
             "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
             "                                                     ",
         }
-        dashboard.section.buttons.val = {}
+        dashboard.section.buttons.val = {
+            dashboard.button("q", "", "<cmd>q<cr>"),
+            dashboard.button("f", "", "<cmd>Telescope find_files<cr>"),
+            dashboard.button("t", "", "<cmd>TodoTelescope<cr>"),
+            dashboard.button("r", "", "<cmd>SessionRestore<cr>"),
+        }
         alpha.setup(dashboard.opts)
         vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
     end,
